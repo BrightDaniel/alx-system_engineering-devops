0c03002aa363fce350d72446825f7bf314a9410c
Crib1212 <igwepatrickchinedu@gmail.com>
2024-01-21 16:00:31 +0100

    push

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Automating my Tasks using Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
